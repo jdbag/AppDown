8965d7079f70c7246800b238d13f2a135e6a56f1
jdbag <emprordz@gmail.com>
2025-12-26 21:01:19 +0100

    Debug: log PI_API_KEY in all API routes

--- api/status.ts
+++ api/status.ts
@@ -1,3 +1,4 @@
+console.log("🔑 PI_API_KEY =", process.env.PI_API_KEY);
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
